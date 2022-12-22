3f9478fa5574bf5afbbc916e3d19959fae47bed6
Mehmet Berkay Coruk <89832806+MehmetBerkayC@users.noreply.github.com>
2022-12-22 13:35:18 +0300

    Rigidboy Movement, Air Jumps

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
-    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
-
+    /// Limited Area and Bounciness 
     [SerializeField] Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
     [SerializeField, Range(0f, 1f)] float bounciness = 0.5f;
-    Vector3 velocity;
+
+
+    [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
+    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
+    [SerializeField, Range(0, 5)] int maxAirJumps = 0;
+
+    Rigidbody body;
+
+    // To see which jump are we at
+    int jumpPhase;
+
+    Vector3 velocity, desiredVelocity;
+    
+    bool desiredJump, onGround;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,11 +36,79 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
 
-        /// Choose one of them for your sphere
+        /// Choose one of them for your sphere (Without Rigidbody)
         // JoystickBehavior(playerInput);
-        BasicBouncySphereWithinArea(playerInput);
+        // BasicBouncySphereWithinArea(playerInput);
+
+        // With Rigidbody
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        // We might end up not invoking FixedUpdate next frame, in which case desiredJump is set back to false and the desire to jump will be forgotten.
+        // We can prevent that by combining the check with its previous value via the boolean OR operation, or the OR assignment.
+        // That way it remains true once enabled until we explicitly set it back to false.
+        desiredJump |= Input.GetButtonDown("Jump");
     }
 
+    // For the physics updates FixedUpdate is prefered
+    // this way the sphere doesn't get jittery when colliding something
+    private void FixedUpdate()
+    {
+        UpdateState();
+
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        // Jump
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+
+        body.velocity = velocity;
+
+        onGround = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void EvaluateCollision (Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            // Assuming a Plane - Set true if normal vector from contact surface is 1(on ground)
+            onGround |= normal.y >= 0.9f; // To be sure we set it >= 0.9f
+        }
+    }
+    void Jump() 
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
+            velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+        }
+    }
+
+    void UpdateState()
+    {
+        velocity = body.velocity;
+        if (onGround)
+        {
+            jumpPhase = 0;
+        }
+    }
 
     void BasicBouncySphereWithinArea(Vector2 playerInput)
     {
@@ -46,7 +131,8 @@ public class MovingSphere : MonoBehaviour
 
         // if new position is not within the rectangle
         if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
-        { // We need to give it a new Vector2 because contains only checks x,y in Vector3 we would sent
+        {
+            // We need to give it a new Vector2 because contains only checks x,y in Vector3 we would sent
             if (newPosition.x < allowedArea.xMin)
             {
                 newPosition.x = allowedArea.xMin; // Make it stay inside
@@ -71,7 +157,6 @@ public class MovingSphere : MonoBehaviour
 
         transform.localPosition = newPosition;
 
-
     }
 
 
@@ -88,5 +173,6 @@ public class MovingSphere : MonoBehaviour
         transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
 
     }
+
 
 }
