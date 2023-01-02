9289cff6e1e85ddf8bc764728c6d72aeb6b1de80
Mehmet Berkay Coruk <89832806+MehmetBerkayC@users.noreply.github.com>
2023-01-02 16:05:15 +0300

    Made a map for experimenting the camera behaviors

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -96,7 +96,7 @@ public class MovingSphere : MonoBehaviour
             // if we just take the camera's transform direction,
             // camera's Y movement affects sphere's movement
             //desiredVelocity = playerInputSpace.TransformDirection(playerInput.x, 0f, playerInput.y) * maxSpeed;
-            
+
             // Movement independent of Camera's Y Axis 
             Vector3 forward = playerInputSpace.forward;
             forward.y = 0f;
@@ -105,7 +105,7 @@ public class MovingSphere : MonoBehaviour
             Vector3 right = playerInputSpace.right;
             right.y = 0f;
             right.Normalize();
-            
+
             desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
         }
         else // Keep Player input in world space
